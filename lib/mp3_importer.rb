35655a0cfc496b99b28d0589bcbd374be3c4286a
ashley <ashleyhernandez810.ah@gmail.com>
2020-11-13 23:45:55 -0500

    commit tree d9368a6f04584b5f64fcde4f257a7b24f0b1f5d6

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,20 @@
+class MP3Importer
+    attr_accessor :directory, :path, :file
+    @@files=[]
+def initialize(path)
+@path=path
+end 
+def files
+    @directory=Dir.entries(path)
+    @directory.each do |f| 
+    @@files << f if f.include?("mp3")
+end 
+@@files
+end 
+
+def import
+files.map do |song|
+    Song.new_by_filename(song)
+end 
+end 
+end
---
+++ lib/song.rb
@@ -0,0 +1,26 @@
+class Song
+        attr_accessor :name, :artist
+        @@all=[]
+    def initialize(name)
+        @name=name
+        @@all<<self
+    end 
+    def self.all
+        @@all
+    end
+
+    def self.new_by_filename(file_name)
+       song_name= file_name.split(" - ")[1]
+       artist_instance=file_name.split(" - ")[0]
+       new_song=Song.new(song_name)
+       new_song.artist= Artist.new(artist_instance)
+       return new_song
+    end
+
+    def artist_name=(name)
+          artist= Artist.find_or_create_by_name(name)
+          self.artist=artist
+          artist.add_song(self)
+    end 
+
+end
